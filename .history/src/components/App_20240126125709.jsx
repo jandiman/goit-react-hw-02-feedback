f4926a498d1a1c3527ddcff8ceacad1691004b57
Jan Dilbert Ampuan <92305240+jandiman@users.noreply.github.com>
2024-01-26 13:03:32 +0800

    Added codes for feedback options

--- .history/src/components/App_20240126125709.jsx
+++ .history/src/components/App_20240126125709.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
-import { Notifs } from './Notifs/Notifs';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   state = {
@@ -58,7 +58,7 @@ export class App extends Component {
               positivePercentage={positivePercentage}
             />
           ) : (
-            <Notifs message="There is no feedback" />
+            <Notification message="There is no feedback" />
           )}
         </Section>
       </div>
--- .history/src/components/App_20240126130030.jsx
+++ .history/src/components/App_20240126130030.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
-import { Notifs } from './Notifs/Notifs';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   state = {
